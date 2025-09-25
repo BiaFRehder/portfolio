61c708ff9fa71d5fa5c3d9c7a072c626c58a6378
BiaFucuda <161872803+BiaFRehder@users.noreply.github.com>
2025-09-25 14:33:44 -0300

    Estilizando a sidebar

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -3,10 +3,13 @@ import { P } from './styles'
 export type Props = {
   children: string
   tipo?: 'principal' | 'secundario' // Com o pipe você pode limitar o valor da prop. Poderia ser string, mas a prop receberia qualquer string. Assim é mais seguro
+  fontSize?: number
 }
 
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
-  <P tipo={tipo}>{children}</P>
+const Paragrafo = ({ children, tipo = 'principal', fontSize }: Props) => (
+  <P fontSize={fontSize} tipo={tipo}>
+    {children}
+  </P>
 )
 
 export default Paragrafo
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/BiaFRehder.png" />
-    <Titulo fontSize={20}>Bia Fucuda Rehder</Titulo>
-    {/* Depois de definir o children, a propriedade consegue receber coisas, nesse caso, uma string */}
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Bia Fucuda Rehder</Titulo>
+      {/* Depois de definir o children, a propriedade consegue receber coisas, nesse caso, uma string */}
+      <Paragrafo tipo="secundario" fontSize={16}>
+        BiaFRehder
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
